b7b4a525affe1365361ed45bc376b86af86e7cd1
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-01-12 15:53:59 -0700

    Updated read more snippet location

--- dcc-widgets/dcc-prayerWall.js
+++ dcc-widgets/dcc-prayerWall.js
@@ -33,7 +33,7 @@ function addPrayer(prayerID) {
 }
 
 function loadPrayerWall() {
-    console.log('Prayer Wall v0.23.01.12.1');
+    console.log('Prayer Wall v0.23.01.12.3');
     /*Initialize local storage for prayed-for prayers*/
     var prayedForPrayers = JSON.parse(localStorage.getItem("prayedForPrayers"));
     if(prayedForPrayers == null) {
@@ -88,20 +88,21 @@ function loadPrayerWall() {
                 divHTML = divHTML+`</div>`;
                 document.getElementsByTagName("dcc-PrayerWall")[0].innerHTML = divHTML;
 
-                const prayerDescriptions = document.querySelectorAll('.prayer-description')
-
-                prayerDescriptions.forEach(pDescription => {
-                    if (pDescription.offsetHeight < pDescription.scrollHeight) {
-                            pDescription.innerHTML += '<button class="prayer-read-more" onclick=\'event.target.parentNode.classList.add("prayer-description-full");event.target.style.display="none";\'>Read More</button>';
-                        }
-                });
-
             } else {
                 /*Report something went wrong - failure response from server*/
                 console.log('Prayer Wall response failure. Returning error.');
                 divHTML = divHTML+`<p>Sorry, something went wrong. Please try again later.</p>`;
                 document.getElementsByTagName("dcc-PrayerWall")[0].innerHTML = divHTML;
             }
+        })
+        .then(function (readMore) {
+            /* Adds a Read More button to long description boxes */
+            const prayerDescriptions = document.querySelectorAll('.prayer-description')
+                prayerDescriptions.forEach(pDescription => {
+                    if (pDescription.offsetHeight < pDescription.scrollHeight) {
+                        pDescription.innerHTML += '<button class="prayer-read-more" onclick=\'event.target.parentNode.classList.add("prayer-description-full");event.target.style.display="none";\'>Read More</button>';
+                        }
+                });
         })
         .catch(function (fail) {
             /*Report something went wrong - couldn't connect to API*/
