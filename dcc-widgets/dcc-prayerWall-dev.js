1790bf9bdb7e947e41c361cc462bf36eab4351f1
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-01-12 16:07:15 -0700

    Added read more timeout to give DOM loading time

--- dcc-widgets/dcc-prayerWall-dev.js
+++ dcc-widgets/dcc-prayerWall-dev.js
@@ -33,7 +33,7 @@ function addPrayer(prayerID) {
 }
 
 function loadPrayerWall() {
-    console.log('Prayer Wall v0.23.01.12.3');
+    console.log('Prayer Wall v0.23.01.12.4');
     /*Initialize local storage for prayed-for prayers*/
     var prayedForPrayers = JSON.parse(localStorage.getItem("prayedForPrayers"));
     if(prayedForPrayers == null) {
@@ -99,6 +99,7 @@ function loadPrayerWall() {
             }
         })
         .then(function (readMore) {
+            setTimeout(function() {
             console.log('Starting read more buttons')
             const prayerDescriptions = document.querySelectorAll('.prayer-description')
 
@@ -107,7 +108,7 @@ function loadPrayerWall() {
                         console.log('Adding read more button')    
                         pDescription.innerHTML += '<button class="prayer-read-more" onclick=\'event.target.parentNode.classList.add("prayer-description-full");event.target.style.display="none";\'>Read More</button>';
                         }
-                });
+                });}, 200)
         })
         .catch(function (fail) {
             /*Report something went wrong - couldn't connect to API*/
