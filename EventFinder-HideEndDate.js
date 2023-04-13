3a803cd2020d712092885e20b0bed6356862553b
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-04-13 10:07:08 -0700

    Initial

---
+++ EventFinder-HideEndDate.js
@@ -0,0 +1,27 @@
+function waitForShadow(timeToWait,currentRun,maxRun) {
+    if (document.querySelector('mpp-event-finder').shadowRoot) {
+        waitForDateFields(timeToWait,1,maxRun);
+    } else if (maxRun > currentRun-1) {
+        currentRun = currentRun+1;
+        setTimeout(waitForShadow,timeToWait,timeToWait,currentRun,maxRun);
+    }
+}
+
+function waitForDateFields(timeToWait,currentRun,maxRun) {
+    if (document.querySelector('mpp-event-finder').shadowRoot.querySelector('.event-date-range')) {
+        updateDateFields();
+    } else if (maxRun > currentRun-1) {
+        currentRun = currentRun+1;
+        setTimeout(waitForDateFields,timeToWait,timeToWait,currentRun,maxRun);
+    }
+}
+
+function updateDateFields(){
+    var DateFields = document.querySelector('mpp-event-finder').shadowRoot.querySelectorAll('.event-date-range');
+    console.log(DateFields);
+    for (var i = 0; i < DateFields.length; i++) {
+        DateFields.item(i).innerHTML = DateFields.item(i).innerHTML.replace(/- [0-9].*/gm,'');
+    }
+}
+
+window. Onload=waitForShadow(100,1,40);
