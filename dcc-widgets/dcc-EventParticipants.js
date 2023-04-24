6cfd999cfe0c03a7fc13a46c371ffa5d609fc394
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-04-24 10:00:51 -0700

    Event Card Styles

--- dcc-widgets/dcc-EventParticipants.js
+++ dcc-widgets/dcc-EventParticipants.js
@@ -23,23 +23,22 @@ function loadEventParticipants() {
         body: JSON.stringify( params ),
         headers: {'Content-Type': 'application/json'}
     };
-    fetch( 'https://prod-04.westus2.logic.azure.com:443/workflows/9ca3fe0b28004cca9d76207dc75f7dc4/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Ks5VRiZRp-sjVhC6tNRZDmhFj2DzZQXDsNtq7uch4Ac', options)
+    fetch('https://prod-04.westus2.logic.azure.com:443/workflows/9ca3fe0b28004cca9d76207dc75f7dc4/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Ks5VRiZRp-sjVhC6tNRZDmhFj2DzZQXDsNtq7uch4Ac', options)
         .then(function (response) {return response.json();})
         .then(function (data) {
             /*Start DIV writeback*/
             console.log(data.status);
             if(data.status=="success"){
                 divHTML = `<div id="event-participants">`;
-                
                 data.EventParticipants[0].forEach((participant) => {
                     divHTML = divHTML+`<div class="participant-card">
-                        <table>
-                        <tr><td class="participant-name">${participant.Participant_Name}</td></tr>
-                        <tr><td class="participant-contact">${participant.Mobile_Phone} ${participant.Email_Address}</td></tr>
-                        <tr><td class="participant-paid">Total Paid: ${participant.Paid_Total}</td></tr>
-                        <tr><td class="participant-remaining">Payment Remaining: ${participant.Remaining}</td></tr>
-                        <tr><td class="participant-options">Registration Options: ${participant.Selected_Options}</td></tr>
-                        </table>
+                        <div class="participant-name">${participant.Participant_Name}</div>
+                        <div class="registration-date">Registered at ${participant.Registration_Date}</div>`;
+                    if(participant.Mobile_Phone){divHTML = divHTML+`<div class="participant-mobile"><a href="tel:${participant.Mobile_Phone}" target="_blank">${participant.Mobile_Phone}</a></div>`;}
+                    if(participant.Email_Address){divHTML = divHTML+`<div class="participant-email"><a href="mailto:${participant.Email_Address}" target="_blank">${participant.Email_Address}</a></div>`;}
+                    divHTML = divHTML+`                       
+                        <div class="participant-paid">Paid: $${participant.Paid_Total}</div>
+                        <div class="participant-options">Selected Options: ${participant.Selected_Options}</div>
                         </div>`;
                     }
                 )
@@ -59,8 +58,7 @@ function loadEventParticipants() {
             console.log('Event Participant connection failure. Returning error.');
             divHTML = `<p>Sorry, something went wrong. Please try again later.</p>`;
             document.getElementsByTagName("dcc-EventParticipants")[0].innerHTML = divHTML;
-        } 
-    
+        })
 }
 
 window.onload = setTimeout(loadEventParticipants, 500);
