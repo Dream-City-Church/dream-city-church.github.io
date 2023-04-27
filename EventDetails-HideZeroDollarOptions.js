77886d6dfe9ce02c0a2da1a1acf31a4566ea8e75
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-04-27 15:28:03 -0700

    Hide prices, don't log events

---
+++ EventDetails-HideZeroDollarOptions.js
@@ -0,0 +1,29 @@
+function waitForShadow(timeToWait,currentRun,maxRun) {
+    if (document.querySelector('mpp-event-details').shadowRoot) {
+        waitForOptionPrices(timeToWait,1,maxRun);
+    } else if (maxRun > currentRun-1) {
+        currentRun = currentRun+1;
+        setTimeout(waitForShadow,timeToWait,timeToWait,currentRun,maxRun);
+    }
+}
+
+function waitForOptionPrices(timeToWait,currentRun,maxRun) {
+    if (document.querySelector('mpp-event-details').shadowRoot.querySelector('.option-price')) {
+        updateOptionPrices();
+    } else if (maxRun > currentRun-1) {
+        currentRun = currentRun+1;
+        setTimeout(waitForOptionPrices,timeToWait,timeToWait,currentRun,maxRun);
+    }
+}
+
+function updateOptionPrices(){
+    var OptionPrices = document.querySelector('mpp-event-details').shadowRoot.querySelectorAll('.option-price');
+    console.log(OptionPrices);
+    for (var i = 0; i < OptionPrices.length; i++) {
+        if(OptionPrices.item(i).innerHTML=="$.0.00"){
+            OptionPrices.item(i).innerHTML="";
+        }
+    }
+}
+
+window. Onload=waitForShadow(100,1,40);
