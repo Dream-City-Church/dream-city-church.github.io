b4c8b36376529a229ef0dd8ac8d5cf5d2aa2f524
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-05-01 13:30:16 -0700

    Initial campus picker

---
+++ EventFinder-ShowCampus.js
@@ -0,0 +1,19 @@
+function waitForShadow(timeToWait,currentRun,maxRun) {
+    if (document.querySelector('mpp-event-finder').shadowRoot) {
+        ShowCampusPicker();
+    } else if (maxRun > currentRun-1) {
+        currentRun = currentRun+1;
+        setTimeout(waitForShadow,timeToWait,timeToWait,currentRun,maxRun);
+    }
+}
+
+function ShowCampusPicker(){
+    var FormFields = document.querySelector('mpp-event-finder').shadowRoot.querySelectorAll('.mppw-form-field mppw-form-field__static-container');
+    /* console.log(DateFields); */
+    for (var i = 0; i < FormFields.length; i++) {
+        if(FormFields.item(i).getElementsByClassName.style.display="none") {}
+            FormFields.item(i).getElementsByClassName.style.display="inherit";
+    }
+}
+
+window. Onload=waitForShadow(100,1,40);
