7a5a7ceb1eea707e32735d884bac0e63b6d73dfd
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-05-01 13:48:02 -0700

    Merge

--- EventFinder-HideEndDate.js
+++ EventFinder-HideEndDate.js
@@ -10,9 +10,19 @@ function waitForShadow(timeToWait,currentRun,maxRun) {
 function waitForDateFields(timeToWait,currentRun,maxRun) {
     if (document.querySelector('mpp-event-finder').shadowRoot.querySelector('.event-date-range')) {
         updateDateFields();
+        ShowCampusPicker();
     } else if (maxRun > currentRun-1) {
         currentRun = currentRun+1;
         setTimeout(waitForDateFields,timeToWait,timeToWait,currentRun,maxRun);
+    }
+}
+
+function ShowCampusPicker(){
+    var FormFields = document.querySelector('mpp-event-finder').shadowRoot.querySelectorAll('.mppw-form-field mppw-form-field__static-container');
+    console.log('Showing campus picker')
+    for (var i = 0; i < FormFields.length; i++) {
+        if(FormFields.item(i).style.display==="none") {}
+            FormFields.item(i).style.display="inherit";
     }
 }
 
