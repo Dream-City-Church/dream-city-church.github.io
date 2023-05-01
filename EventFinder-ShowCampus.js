3217578ef25cd050183b015071334d137450bb2f
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-05-01 13:39:41 -0700

    Fix selector

--- EventFinder-ShowCampus.js
+++ EventFinder-ShowCampus.js
@@ -11,8 +11,8 @@ function ShowCampusPicker(){
     var FormFields = document.querySelector('mpp-event-finder').shadowRoot.querySelectorAll('.mppw-form-field mppw-form-field__static-container');
     console.log('Showing campus picker')
     for (var i = 0; i < FormFields.length; i++) {
-        if(FormFields.item(i).getElementsByClassName.style.display="none") {}
-            FormFields.item(i).getElementsByClassName.style.display="inherit";
+        if(FormFields.item(i).style.display="none") {}
+            FormFields.item(i).style.display="inherit";
     }
 }
 
