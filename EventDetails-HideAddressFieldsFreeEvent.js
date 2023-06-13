f8472d5715add0f3cd5f1789206283b7e9a4ed23
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-06-13 13:10:58 -0700

    added logging

--- EventDetails-HideAddressFieldsFreeEvent.js
+++ EventDetails-HideAddressFieldsFreeEvent.js
@@ -9,6 +9,7 @@ function waitForShadowHideAddress(timeToWait,currentRun,maxRun) {
 
 function waitForRegistrationContainer(timeToWait,currentRun,maxRun) {
     if (document.querySelector('mpp-event-details').shadowRoot.querySelector('#registrationTotalContainer').style.display == 'none') {
+        console.log('Reg container found');
         updateAddressFields();
     } else if (maxRun > currentRun-1) {
         currentRun = currentRun+1;
