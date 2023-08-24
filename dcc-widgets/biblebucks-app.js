d810e0fe3f44676069b22301e7adf694364245cd
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-08-24 16:36:09 -0700

    one line

--- dcc-widgets/biblebucks-app.js
+++ dcc-widgets/biblebucks-app.js
@@ -42,10 +42,7 @@ function onScanSuccess(decodedText, decodedResult) {
     console.warn(`Code scan error = ${error}`);
   }
   
-  let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: {width: 250, height: 250} },
-    /* verbose= */ false);
+  let html5QrcodeScanner = new Html5QrcodeScanner("reader",{ fps: 10, qrbox: {width: 250, height: 250} },/* verbose= */ false);
   html5QrcodeScanner.render(onScanSuccess, onScanFailure);
   
 
