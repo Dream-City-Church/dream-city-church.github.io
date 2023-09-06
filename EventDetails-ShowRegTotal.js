b01fc4f3f3861d38799cf4b449fd5d1318ed1e1f
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-09-06 16:18:22 -0700

    Unhide if starts greater than 0

--- EventDetails-ShowRegTotal.js
+++ EventDetails-ShowRegTotal.js
@@ -7,21 +7,24 @@ function waitForShadowRegTotal(timeToWait,currentRun,maxRun) {
         }
     }
     
-    function waitForRegTotalContainer(timeToWait,currentRun,maxRun) {
-        const regTotalValue = document.querySelector('mpp-event-details').shadowRoot.querySelector("#registrationTotalValue");
-        const regTotalContainer = document.querySelector('mpp-event-details').shadowRoot.querySelector("#registrationTotalContainer");
+function waitForRegTotalContainer(timeToWait,currentRun,maxRun) {
+    const regTotalValue = document.querySelector('mpp-event-details').shadowRoot.querySelector("#registrationTotalValue");
+    const regTotalContainer = document.querySelector('mpp-event-details').shadowRoot.querySelector("#registrationTotalContainer");
 
-        if (regTotalValue) {
-            const observer = new MutationObserver(() => {
-                if (regTotalValue.innerHTML != "$0.00"){
-                    regTotalContainer.style.display = 'block';
-                }
-            });
-            observer.observe(regTotalValue, { characterData: true, subtree: true, childList: true, attributes: true });  
-        } else if (maxRun > currentRun-1) {
-            currentRun = currentRun+1;
-            setTimeout(waitForRegTotalContainer,timeToWait,timeToWait,currentRun,maxRun);
+    if (regTotalValue) {
+        if (regTotalValue.innerHTML != "$0.00"){
+            regTotalContainer.style.display = 'block';
         }
+        const observer = new MutationObserver(() => {
+            if (regTotalValue.innerHTML != "$0.00"){
+                regTotalContainer.style.display = 'block';
+            }
+        });
+        observer.observe(regTotalValue, { characterData: true, subtree: true, childList: true, attributes: true });
+    } else if (maxRun > currentRun-1) {
+        currentRun = currentRun+1;
+        setTimeout(waitForRegTotalContainer,timeToWait,timeToWait,currentRun,maxRun);
     }
+}
     
 window.onload=waitForShadowRegTotal(100,1,40);
