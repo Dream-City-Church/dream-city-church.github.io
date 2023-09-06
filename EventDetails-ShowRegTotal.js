d1b943e62b423d7ff72f56331f12e2a50d9db11f
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2023-09-06 16:09:05 -0700

    d'oh

--- EventDetails-ShowRegTotal.js
+++ EventDetails-ShowRegTotal.js
@@ -8,16 +8,16 @@ function waitForShadowRegTotal(timeToWait,currentRun,maxRun) {
     }
     
     function waitForRegTotalContainer(timeToWait,currentRun,maxRun) {
-        if (document.querySelector('mpp-event-details').shadowRoot.querySelector('#registrationTotalValue')) {
-            
-                const regTotalValue = document.querySelector("#registrationTotalValue");
-                const observer = new MutationObserver(() => {
-                    if (regTotalValue.innerHTML != "$0.00"){
-                            document.querySelector("#registrationTotalValue").style.display = 'block';
-                    }
-                });
-                observer.observe(regTotalValue, { characterData: true, subtree: true, childList: true, attributes: true });  
+        const regTotalValue = document.querySelector('mpp-event-details').shadowRoot.querySelector("#registrationTotalValue");
+        const regTotalContainer = document.querySelector('mpp-event-details').shadowRoot.querySelector("#registrationTotalContainer");
 
+        if (regTotalValue) {
+            const observer = new MutationObserver(() => {
+                if (regTotalValue.innerHTML != "$0.00"){
+                    regTotalContainer.style.display = 'block';
+                }
+            });
+            observer.observe(regTotalValue, { characterData: true, subtree: true, childList: true, attributes: true });  
         } else if (maxRun > currentRun-1) {
             currentRun = currentRun+1;
             setTimeout(waitForRegTotalContainer,timeToWait,timeToWait,currentRun,maxRun);
