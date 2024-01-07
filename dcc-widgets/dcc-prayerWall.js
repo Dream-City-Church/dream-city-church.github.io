08618ea95ac8f46da6fad3383d9e006ea2064df8
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2024-01-07 11:28:39 -0700

    Add Read More button to long prayer descriptions

--- dcc-widgets/dcc-prayerWall.js
+++ dcc-widgets/dcc-prayerWall.js
@@ -190,7 +190,8 @@ function loadPrayers() {
             /* Adds a Read More button to long description boxes */
             const prayerDescriptions = document.querySelectorAll('.prayer-description')
                 prayerDescriptions.forEach(pDescription => {
-                    if (pDescription.offsetHeight < pDescription.scrollHeight-2) {
+                    var buttons = pDescription.getElementsByTagName('button');
+                    if (pDescription.offsetHeight < pDescription.scrollHeight-2 && buttons.length < 1) {
                         pDescription.innerHTML += '<button class="prayer-read-more" onclick=\'event.target.parentNode.classList.add("prayer-description-full");event.target.style.display="none";\'>Read More</button>';
                         }
                 });
