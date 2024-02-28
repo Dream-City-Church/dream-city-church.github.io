c83d848ee3068f6dee667f69f59b3a70e6c0f708
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2024-02-28 16:47:07 -0700

    Refactor event metrics display and add event thumbnail support

--- dcc-widgets/dcc-EventMetrics.js
+++ dcc-widgets/dcc-EventMetrics.js
@@ -47,8 +47,35 @@ function getCurrentEvents() {
                 return PromiseRejectionEvent('Error: ' + response.status);
             }})
         .then(function (data) {
-            divHTML = `
-            `;
+            if(data.Events.length > 0){
+                /* for each event, create div with event image, title, and date */
+                divHTML = `<div id="dcc-eventmetrics">`;
+
+                for (var i = 0; i < data.Events.length; i++) {
+                    if(data.Events[i].File_GUID == '' || data.Events[i].File_GUID == null) {
+                        divHTML += `<div class="dcc-eventmetrics-event">
+                        <div class="event-thumbnail empty"></div>
+                        <div class="event-description">
+                            <h3>${data.Events[i].Event_Title}</h3>
+                            <div class="date">${data.Events[i].Event_Start_Date}</div>
+                        </div>
+                        </div>`;
+                    } else {
+                        divHTML += `<div class="dcc-eventmetrics-event">
+                        <img src="https://my.dreamcitychurch.us/ministryplatformapi/files/${data.Events[i].File_GUID}" alt="${data.Events[i].Event_Title}" class="event-thumbnail" />
+                        <div class="event-description">
+                            <h3>${data.Events[i].Event_Title}</h3>
+                            <div class="date">${data.Events[i].Event_Start_Date}</div>
+                        </div>
+                        </div>`;
+                    }
+                }
+
+                document.getElementsByTagName("dcc-EventMetrics")[0].innerHTML = divHTML;
+
+            } else {
+                divHTML = `<div id="dcc-eventmetrics"><div id="status-text"><p>No events found.</p></div></div>`;
+            }
         })
         .catch(function (error) {
             divHTML = `<div id="dcc-eventmetrics"><div id="tatus-text"><p>Uh oh, something went wrong.<br /><br />We're having trouble communicating with our services. Please try again later.</p></div></div>`;
