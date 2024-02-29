3889c5cde5f82b27c260179d32817167d96d0ac1
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2024-02-29 12:48:37 -0700

    Add responsive meta tag and overlay styles***
    ***Update event metrics styles and add form***
    
    ***Update dark mode styles

--- dcc-widgets/dcc-EventMetrics.js
+++ dcc-widgets/dcc-EventMetrics.js
@@ -49,39 +49,42 @@ function getCurrentEvents() {
         .then(function (data) {
             if(data.Events.length > 0){
                 /* for each event, create div with event image, title, and date */
-                divHTML = `<div id="dcc-eventmetrics">`;
+                divHTML = `<div id="dcc-eventmetrics"><div id="overlay"></div>`;
 
                 for (var i = 0; i < data.Events.length; i++) {
-                        divHTML += `<div class="dcc-eventmetrics-event">
-                        <div class="event-thumbnail" style="background-image: url(https://my.dreamcitychurch.us/ministryplatformapi/files/${data.Events[i].File_GUID});"></div>
-                        <div class="event-description" style="background-image:">
+                        divHTML += `<div class="dcc-eventmetrics-event" id="eventmetrics-event-${data.Events[i].Event_ID}" >
+                        <div class="event-thumbnail" style="background-image: url(https://my.dreamcitychurch.us/ministryplatformapi/files/${data.Events[i].File_GUID});" onclick="toggleEventMetrics(${data.Events[i].Event_ID},'open','thumbnail')"></div>
+                        <div class="event-description"  onclick="toggleEventMetrics(${data.Events[i].Event_ID},'open','description')">
                             <h3>${data.Events[i].Event_Title}</h3>
                             <div class="date">${data.Events[i].Event_Start_Date}</div>
-                        </div>`;
+                        </div>
+                        <div class="event-metrics" id="event-${data.Events[i].Event_ID}-metrics">`;
                         if (data.Events[i].Event_Metrics.length > 0){
-                            divHTML += `<div class="event-metrics">`;
                             for (var j = 0; j < data.Events[i].Event_Metrics.length; j++) {
                                 divHTML += `<div class="metric">
                                     <div class="event-metric">${data.Events[i].Event_Metrics[j].Metric_Title}: ${data.Events[i].Event_Metrics[j].Numerical_Value}</div>
                                 </div>`;
                             }
-                            divHTML += `</div>`;
                         }
                         /* Add form with dropdown for selecting a Metric and an input for the metric value */
                         divHTML += `
+                        </div>
                         <div class="event-metric-form">
                             <form id="event-metric-form-${i}">
-                                <select name="metric" id="metric-${i}">
+                                <select name="metric" id="metric-${i}" required >
                                     <option value="0">Select a Metric</option>`;
                                     for (var k = 0; k < data.Metrics.length; k++) {
                                         divHTML += `<option value="${data.Metrics[k].Metric_ID}">${data.Metrics[k].Metric_Title}</option>`;
                                     }
                                     divHTML += `
                                 </select>
-                                <input type="number" id="metric-value-${i}" name="metric-value" placeholder="Enter Value">
-                                <button type="button" onclick="submitMetric(${i}, ${data.Events[i].Event_ID})">Submit</button>
+                                <input type="number" id="metric-value-${i}" name="metric-value" placeholder="Enter Value" required >
+                                <textarea type="text" id="metric-notes-${i}" name="metric-notes" placeholder="Any additional notes" maxlength="250" ></textarea>
+                                <button type="button" onclick="submitMetric(${i},${data.Events[i].Event_ID})">Submit</button>
                             </form>
-                        </div></div>`;
+                        </div>
+                        <div class="close-event-metrics" onclick="toggleEventMetrics(${data.Events[i].Event_ID},'close','close button')">close</div>
+                        </div>`;
                 }
 
                 document.getElementsByTagName("dcc-EventMetrics")[0].innerHTML = divHTML;
@@ -96,5 +99,87 @@ function getCurrentEvents() {
                 document.getElementsByTagName("dcc-EventMetrics")[0].innerHTML = divHTML;
         })
     }
+
+function submitMetric(Form_ID,Event_ID) {
+    console.log('submitting metric');
+    /* Get Metric and Value from Form */
+    var metricForm = document.getElementById('event-metric-form-'+Form_ID);
+    var metricId = document.getElementById('metric-'+Form_ID).value;
+    var metricText = document.getElementById('metric-'+Form_ID).options[document.getElementById('metric-'+Form_ID).selectedIndex].text;
+    var metricValue = document.getElementById('metric-value-'+Form_ID).value;
+    var metricNotes = document.getElementById('metric-notes-'+Form_ID).value;
+
+    document.getElementById('metric-'+Form_ID).disabled = true;
+    document.getElementById('metric-value-'+Form_ID).disabled = true;
+    document.getElementById('metric-notes-'+Form_ID).disabled = true;
+
+    /* Check if Metric and Value are valid */
+    if (metricId == 0 || metricValue == "" || metricValue < 1) {
+        document.getElementById('metric-'+Form_ID).disabled = false;
+        document.getElementById('metric-value-'+Form_ID).disabled = false;
+        document.getElementById('metric-notes-'+Form_ID).disabled = false;
+        alert("Please select a metric and enter a value greater than 0.");
+        return;
+    }
+
+    const params = {
+        "User_ID": userId,
+        "Event_ID": Event_ID,
+        "Metric_ID": metricId,
+        "Metric_Value": metricValue,
+        "Metric_Notes": metricNotes
+    };
+    const options = {
+        method: 'POST',
+        body: JSON.stringify( params ),
+        headers: {'Content-Type': 'application/json'}
+    };
+    fetch('https://prod-12.westus2.logic.azure.com:443/workflows/835aa031af4d4836ae7e3ba8d5f43586/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=PvI4ZCT-K8otlzUMFjHjSkl1VICiC8Aug6zK2NGdv1E', options)
+        .then(function (response) {
+            console.log(response.status);
+            /* If status code 200 */
+            if (response.status == 200) {
+                return;
+            } else {
+                /* If status code is not 200 */
+                return PromiseRejectionEvent('Error: ' + response.status);
+            }})
+        .then(function (data) {
+            divHTML = document.getElementById('event-'+Event_ID+'-metrics').innerHTML;
+            divHTML += `<div class="metric">
+                <div class="event-metric">${metricText}: ${metricValue}</div>`;
+
+            document.getElementById('event-'+Event_ID+'-metrics').innerHTML = divHTML;
+
+            metricForm.reset();
+            document.getElementById('metric-'+Form_ID).disabled = false;
+            document.getElementById('metric-value-'+Form_ID).disabled = false;
+            document.getElementById('metric-notes-'+Form_ID).disabled = false;
+        })
+        .catch(function (error) {
+            console.log(error);
+            alert("Uh oh, something went wrong. We're having trouble communicating with our services. Please try again later.");
+
+            document.getElementById('metric-'+Form_ID).disabled = false;
+            document.getElementById('metric-value-'+Form_ID).disabled = false;
+            document.getElementById('metric-notes-'+Form_ID).disabled = false;
+        })
+        
+}
+
+function toggleEventMetrics(eventId, action, source) {
+    console.log('toggling event metrics from '+source)
+    if (action == "close") {
+        console.log('closing event metrics ' + eventId)
+        document.getElementById('eventmetrics-event-'+eventId).classList.remove('open');
+        document.getElementById('overlay').classList.remove('overlay-active');
+        document.getElementById('overlay').setAttribute('onclick', 'return();');
+    } else if (action == "open") {
+        console.log('opening event metrics ' + eventId)
+        document.getElementById('eventmetrics-event-'+eventId).classList.add('open');
+        document.getElementById('overlay').classList.add('overlay-active');
+        document.getElementById('overlay').setAttribute('onclick', 'toggleEventMetrics('+eventId+',\'close\',\'overlay\')');
+    }
+}
 
 window.onload = setTimeout(getCurrentEvents, 500);
