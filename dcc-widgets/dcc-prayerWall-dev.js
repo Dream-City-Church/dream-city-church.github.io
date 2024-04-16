2f9a1ab1ea9fa8ddda1fd4d32d8c1feddc2eb3e5
sswinford-dcc <61439805+sswinford-dcc@users.noreply.github.com>
2024-04-15 17:00:57 -0700

    Refactor prayer wall loading skeleton styles and update prayer wall version number and interval for checking new prayers

--- dcc-widgets/dcc-prayerWall-dev.js
+++ dcc-widgets/dcc-prayerWall-dev.js
@@ -125,15 +125,16 @@ function loadPrayerWall() {
 function loadPrayers() {
     divHTML = `
         <div class="prayer-card prayer-wall-skeleton">
-            <div class="prayer-name"><sl-skeleton effect="sheen"></sl-skeleton></div>
+            <div class="prayer-name"><sl-skeleton effect="pulse"></sl-skeleton></div>
             <div class="prayer-date"><sl-skeleton effect="sheen"></sl-skeleton></div>
             <div class="prayer-description">
+                <sl-skeleton effect="pulse"></sl-skeleton>
                 <sl-skeleton effect="sheen"></sl-skeleton>
-                <sl-skeleton effect="sheen"></sl-skeleton>
+                <sl-skeleton effect="pulse"></sl-skeleton>
                 <sl-skeleton effect="sheen"></sl-skeleton>
             </div>
             <div class="prayer-action">
-                <sl-skeleton effect="sheen"></sl-skeleton>
+                <sl-skeleton effect="pulse"></sl-skeleton>
             </div>
         </div>
     `;
