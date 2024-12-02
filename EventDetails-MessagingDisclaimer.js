d21aad0db60a30e9155a347847ff1e9a9763a3b0
Stephan Swinford <sswinford@dreamcitychurch.us>
2024-12-02 15:43:20 -0700

    Refactor SMS disclaimer insertion logic to improve selector handling and prevent duplicate entries

--- EventDetails-MessagingDisclaimer.js
+++ EventDetails-MessagingDisclaimer.js
@@ -1,7 +1,6 @@
 // Description: This script will wait for the shadowroot to load then add a disclaimer to the event details page for SMS messaging.
 
 function waitForShadow(timeToWait,currentRun,maxRun) {
-    console.log('waiting for shadow');
     if (document.querySelector('mpp-event-details').shadowRoot) {
         waitForContactInfoForm(timeToWait,1,maxRun);
     } else if (maxRun > currentRun-1) {
@@ -11,7 +10,6 @@ function waitForShadow(timeToWait,currentRun,maxRun) {
 }
 
 function waitForContactInfoForm(timeToWait,currentRun,maxRun) {
-    console.log('waiting for contact info form');
     if (document.querySelector('mpp-event-details').shadowRoot.querySelector('#contactInfoForm')) {
         insertSmsDisclaimer();
     } else if (maxRun > currentRun-1) {
@@ -21,21 +19,23 @@ function waitForContactInfoForm(timeToWait,currentRun,maxRun) {
 }
 
 function insertSmsDisclaimer(){
-    console.log('inserting sms disclaimer');
     var smsDisclaimer = `<div id="messaging-disclaimer" style="
         font-size: 0.75rem;
         margin-top: 0.5rem;
         line-height: 1rem;
         ">By providing your phone number, you agree to receive text messages from Dream City Church in accordance with our <a href="">Terms of Service</a> and <a href="">Privacy Policy</a>. Reply STOP to cancel. Msg rates may apply.</div>`;
     var ContactInfoForm = document.querySelector('mpp-event-details').shadowRoot.querySelector('#parentInfoForm');
-    // if ContactInfoForm is not found, try a different selector
-    if (!ContactInfoForm) {
+
+    // Check if #parentInfoForm has inline style of display:none, try a different selector
+    if (ContactInfoForm.style.display == 'none') {
         ContactInfoForm = document.querySelector('mpp-event-details').shadowRoot.querySelector('#contactInfoForm');
     }
+    
     // Check if #messaging-disclaimer has already been inserted
     if (ContactInfoForm.querySelector('#messaging-disclaimer')) {
         return;
     }
+
     // insert disclaimer at end of ContactInfoForm container
     ContactInfoForm.insertAdjacentHTML('beforeend',smsDisclaimer);    
 }
