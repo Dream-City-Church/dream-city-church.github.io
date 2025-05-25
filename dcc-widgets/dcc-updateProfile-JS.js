4cd31dcf9164ef4c01a86fad5a552919574d4536
Stephan Swinford <sswinford@dreamcitychurch.us>
2025-05-25 11:21:07 -0700

    Refactor profile update script: add updateSelectLists function to trigger change events for wa-select elements

--- dcc-widgets/dcc-updateProfile-JS.js
+++ dcc-widgets/dcc-updateProfile-JS.js
@@ -6,6 +6,22 @@
 console.log('dcc-updateProfile-JS loaded');
 // All Web Awesome components on the page are ready!
 
+const updateSelectLists = async () => {
+    await customElements.whenDefined('wa-select');
+    console.log('Web Awesome select components are ready!');
+
+    // Find any wa-select elements that have a value attribute set and trigger a change event to force visual update
+    const selects = document.querySelectorAll('wa-select[value]');
+    selects.forEach(select => {
+        const value = select.getAttribute('value');
+        if (value) {
+            console.log(`Triggering change for wa-select with value: ${value}`);
+            select.value = value; // Set the value to trigger the change
+            select.dispatchEvent(new Event('change')); // Dispatch change event
+        }
+    });
+}
+
 const inputListeners = async () => {
     await customElements.whenDefined('wa-input');
     console.log('Web Awesome components are ready!');
@@ -19,7 +35,12 @@ const inputListeners = async () => {
     inputs.forEach(input => {
         input.addEventListener('change', function() {
             const name = this.name;
-            const value = this.select() ? this.selected.value : this.value;
+            // If an input field, use select(), otherwise use value
+            if (this.tagName === 'WA-INPUT' || this.tagName === 'WA-TEXTAREA') {
+                const value = this.select();
+            } else if (this.tagName === 'WA-SELECT') {
+                const value = this.value;
+            }
             const dataTable = this.getAttribute('data-table');
 
             console.log(`Updating profile: ${name} = ${value}, data-table = ${dataTable}`);
@@ -29,4 +50,5 @@ const inputListeners = async () => {
     );
 };
 
+updateSelectLists();
 inputListeners();
