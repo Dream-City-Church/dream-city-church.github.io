99ef111478560a72ffc0ae8b4041abe4594e900a
Stephan Swinford <sswinford@dreamcitychurch.us>
2025-06-11 13:38:41 -0700

    Add functionality to hide "addAnotherButton" based on blocked ID list from URL parameters

--- EventDetails-ShowRegTotal.js
+++ EventDetails-ShowRegTotal.js
@@ -25,6 +25,30 @@ function waitForRegTotalContainer(timeToWait,currentRun,maxRun) {
         currentRun = currentRun+1;
         setTimeout(waitForRegTotalContainer,timeToWait,timeToWait,currentRun,maxRun);
     }
+
+    // Get the value of "id" parameter from the URL
+        const urlIdParams = new URLSearchParams(window.location.search);
+        const urlId = urlIdParams.get('id');
+
+        const blockedIds = ['90893', '456', '789'];
+
+        console.log('ID from URL:', urlId);
+
+        // Check if the "id" parameter is present and not empty
+        if (urlId) {
+            // If the ID value is in the blocked ID list, add a css style to the head of the page to hide the div with id "addAnotherButton"
+            if (blockedIds.includes(urlId)) {
+                const style = document.createElement('style');
+                style.innerHTML = `
+                    #addAnotherButton {
+                        display: none !important;
+                    }
+                `;
+                document.head.appendChild(style);
+            }
+        }
+        // If the ID value is not in the blocked ID list, do nothing
+
 }
     
 window.onload=waitForShadowRegTotal(100,1,40);
