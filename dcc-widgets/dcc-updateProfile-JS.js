63e1d676799bdbdd1ee3c32947e5bce87979476f
Stephan Swinford <sswinford@dreamcitychurch.us>
2025-07-28 15:44:51 -0700

    Refactor input and checkbox listeners to improve state management and error handling; enhance loading feedback during API data submission

--- dcc-widgets/dcc-updateProfile-JS.js
+++ dcc-widgets/dcc-updateProfile-JS.js
@@ -5,160 +5,6 @@
 // If the field update fails, restore the original value and set the field to error state.
 
 console.log('dcc-updateProfile-JS loaded');
-// All Web Awesome components on the page are ready!
-
-const inputListeners = async () => {
-    await customElements.whenDefined('wa-input');
-
-    const profileForm = document.getElementById('UserProfile');
-    const inputs = profileForm.querySelectorAll('wa-input, wa-textarea');
-
-    // add change event listeners to all wa-input and wa-textarea elements
-    inputs.forEach(input => {
-        input.addEventListener('change', function() {
-            const name = this.name;
-            const value = this.value;
-            const dataTable = this.getAttribute('data-table');
-
-            // Send the updated data to the server. Insert the loading icon into the element until the server responds.
-            // This will allow the user to see that the data is being sent.
-            const loadingIcon = '<i slot="end" class="fa-solid fa-floppy-disk fa-fade" style="color: #bc204b;"></i>';
-            this.appendChild(document.createRange().createContextualFragment(loadingIcon));
-
-            // Create a promise to handle the asynchronous nature of the API call
-            // This will allow us to wait for the API response before proceeding
-            await sendDataToAPI(name, value, dataTable, null);
-            console.log(`Result from sendDataToAPI: ${result}`);
-            // Check the result from the API call
-            if (result === 'success') {
-                this.removeAttribute('error'); // Remove the error state
-                    this.removeChild(this.querySelector('[slot="end"]'));
-                    this.dispatchEvent(new Event('input')); // Trigger input event to update UI
-                } else {
-                    this.setAttribute('error', 'true'); // Set the error state
-                    this.removeChild(this.querySelector('[slot="end"]'));
-                    this.dispatchEvent(new Event('input')); // Trigger input event to update UI
-                }
-            }).catch(error => {
-                console.error(`Error sending data to API: ${error}`);
-                this.setAttribute('error', 'true'); // Set the error state
-                this.removeChild(this.querySelector('[slot="end"]'));
-                this.dispatchEvent(new Event('input')); // Trigger input event to update UI
-            });
-            
-        });
-
-        // for wa-input name=Mobile_Phone, format the phone number like 123-456-7890 as it is typed
-        if (input.name === 'Mobile_Phone') {
-            input.addEventListener('input', function() {
-                let value = this.value.replace(/\D/g, ''); // Remove non-digit characters
-                if (value.length > 3 && value.length <= 6) {
-                    value = `${value.slice(0, 3)}-${value.slice(3)}`;
-                } else if (value.length > 6) {
-                    value = `${value.slice(0, 3)}-${value.slice(3, 6)}-${value.slice(6, 10)}`;
-                }
-                this.value = value;
-            });
-        }
-    }
-    );
-};
-
-const selectListeners = async () => {
-    await customElements.whenDefined('wa-select');
-
-    const profileForm = document.getElementById('UserProfile');
-    const inputs = profileForm.querySelectorAll('wa-select');
-
-    // Find any wa-select elements that have a value attribute set and trigger a change event to force visual update
-    const selects = profileForm.querySelectorAll('wa-select[value]');
-    selects.forEach(select => {
-        const value = select.getAttribute('value');
-        if (value) {
-            console.log(`Triggering change for wa-select with value: ${value}`);
-            select.value = value; // Set the value to trigger the change
-            select.dispatchEvent(new Event('change')); // Dispatch change event
-        }
-    });
-
-    // Add change event listeners to all wa-select elements
-    inputs.forEach(input => {
-        input.addEventListener('change', function() {
-            const name = this.name;
-            const value = this.value;
-            const dataTable = this.getAttribute('data-table');
-
-            // Send the updated data to the server. Insert the loading icon into the element until the server responds.
-            // This will allow the user to see that the data is being sent.
-            const loadingIcon = '<i slot="end" class="fa-solid fa-floppy-disk fa-fade" style="color: #bc204b;"></i>';
-            this.appendChild(document.createRange().createContextualFragment(loadingIcon));
-            
-            // Create a promise to handle the asynchronous nature of the API call
-            // This will allow us to wait for the API response before proceeding
-            sendDataToAPI(name, value, dataTable, null).then(result => {
-                console.log(`Result from sendDataToAPI: ${result}`);
-                // Check the result from the API call
-                if (result === 'success') {
-                    this.removeAttribute('error'); // Remove the error state
-                    this.removeChild(this.querySelector('[slot="end"]'));
-                    this.dispatchEvent(new Event('input')); // Trigger input event to update UI
-                } else {
-                    this.setAttribute('error', 'true'); // Set the error state
-                    this.removeChild(this.querySelector('[slot="end"]'));
-                    this.dispatchEvent(new Event('input')); // Trigger input event to update UI
-                }
-            }).catch(error => {
-                console.error(`Error sending data to API: ${error}`);
-                this.setAttribute('error', 'true'); // Set the error state
-                this.removeChild(this.querySelector('[slot="end"]'));
-                this.dispatchEvent(new Event('input')); // Trigger input event to update UI
-            });
-        });
-    });
-};
-
-const waCheckboxListeners = async () => {
-    await customElements.whenDefined('wa-checkbox');
-
-    const profileForm = document.getElementById('UserProfile');
-    const inputs = profileForm.querySelectorAll('wa-checkbox');
-
-    // Add change event listeners to all wa-checkbox elements
-    inputs.forEach(input => {
-        input.addEventListener('change', function() {
-            const name = this.name;
-            const value = this.checked;
-            const dataTable = this.getAttribute('data-table');
-
-            // Send the updated data to the server. Insert the loading icon into the element until the server responds.
-            // This will allow the user to see that the data is being sent.
-            const loadingIcon = '<i slot="end" class="fa-solid fa-floppy-disk fa-fade" style="color: #bc204b;"></i>';
-            this.appendChild(document.createRange().createContextualFragment(loadingIcon));
-            sendDataToAPI(name, value, dataTable, null);
-            // Remove the loading icon after a short delay to simulate server response
-            setTimeout(() => {
-                this.removeChild(this.querySelector('[slot="end"]')); // Clear the loading icon
-            }, 1000); // Adjust the delay as needed
-        });
-    });
-};
-
-const checkboxListeners = async () => {
-    const profileForm = document.getElementById('UserProfile');
-    const checkboxes = profileForm.querySelectorAll('input[type="checkbox"]');
-
-    checkboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', function() {
-            const action = this.checked;
-            const name = this.name;
-            const value = this.value;
-            const dataTable = this.getAttribute('data-table');
-            const dataAttribute = this.getAttribute('data-attribute-type');
-
-            sendDataToAPI(name, value, dataTable, dataAttribute);
-        });
-    });
-};
 
 // Function for sending data to the API service
 sendDataToAPI = (name, value, dataTable, dataAttribute) => {
@@ -173,6 +19,190 @@ sendDataToAPI = (name, value, dataTable, dataAttribute) => {
         document.dispatchEvent(event);
         return 'success'; // Simulate a successful response
     }, 2000); // Simulate network delay
+};
+
+// Function to handle input changes and send data to the API
+const inputListeners = async () => {
+    await customElements.whenDefined('wa-input');
+    await customElements.whenDefined('wa-select');
+
+    const profileForm = document.getElementById('UserProfile');
+    const inputs = profileForm.querySelectorAll('wa-input, wa-textarea, wa-select');
+
+    // Add focus event listener to all inputs. Store the current value when the input is focused.
+    // On blur, if a change is detected, send the updated data to the server.
+    inputs.forEach(input => {
+        input.addEventListener('focus', function() {
+            // Store the current value in a data attribute for later comparison
+            this.setAttribute('data-original-value', this.value);
+        });
+        input.addEventListener('change', async function() {
+            // Compare the current value with the original value
+            const originalValue = this.getAttribute('data-original-value');
+            if (this.value !== originalValue) {
+                const name = this.name;
+                const value = this.value;
+                const dataTable = this.getAttribute('data-table');
+                // Send the updated data to the server. Insert the loading icon into the element until the server responds.
+                // This will allow the user to see that the data is being sent.
+                const loadingIcon = '<i slot="end" class="fa-solid fa-floppy-disk fa-fade" style="color: #bc204b;"></i>';
+                this.appendChild(document.createRange().createContextualFragment(loadingIcon));
+                // Create a promise to handle the asynchronous nature of the API call
+                // This will allow us to wait for the API response before proceeding
+                try {
+                    const result = await sendDataToAPI(name, value, dataTable, null);
+                    console.log(`Result from sendDataToAPI: ${result}`);
+                    // Check the result from the API call
+                    if (result === 'success') {
+                        this.removeAttribute('error'); // Remove the error state
+                        this.removeChild(this.querySelector('[slot="end"]'));
+                        this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                    } else {
+                        this.setAttribute('error', 'true'); // Set the error state
+                        this.removeChild(this.querySelector('[slot="end"]'));
+                        this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                    }
+                } catch (error) {
+                    console.error(`Error sending data to API: ${error}`);
+                    this.setAttribute('error', 'true'); // Set the error state
+                    this.removeChild(this.querySelector('[slot="end"]'));
+                    this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                }
+            } else {
+                // If the value has not changed, remove the loading icon if it exists
+                const loadingIcon = this.querySelector('[slot="end"]');
+                if (loadingIcon) {
+                    this.removeChild(loadingIcon);
+                }
+            }
+        });
+    });
+
+    // for wa-input name=Mobile_Phone, format the phone number like 123-456-7890 as it is typed
+    if (inputs.name === 'Mobile_Phone') {
+        inputs.addEventListener('input', function() {
+            let value = this.value.replace(/\D/g, ''); // Remove non-digit characters
+            if (value.length > 3 && value.length <= 6) {
+                value = `${value.slice(0, 3)}-${value.slice(3)}`;
+            } else if (value.length > 6) {
+                value = `${value.slice(0, 3)}-${value.slice(3, 6)}-${value.slice(6, 10)}`;
+            }
+            this.value = value;
+        });
+    }
+}
+
+const waCheckboxListeners = async () => {
+    await customElements.whenDefined('wa-checkbox');
+
+    const profileForm = document.getElementById('UserProfile');
+    const inputs = profileForm.querySelectorAll('wa-checkbox');
+
+    // Add change event listeners to all wa-checkbox elements
+    inputs.forEach(input => {
+        input.addEventListener('focus', function() {
+            // Store the current checked state in a data attribute for later comparison
+            this.setAttribute('data-original-checked', this.checked);
+        });
+        // On change, if a change is detected, send the updated data to the server.
+        input.addEventListener('change', async function() {
+            // Compare the current value with the original value
+            const originalValue = this.getAttribute('data-original-value');
+            if (this.value !== originalValue) {
+                const name = this.name;
+                const value = this.value;
+                const dataTable = this.getAttribute('data-table');
+                // Send the updated data to the server. Insert the loading icon into the element until the server responds.
+                // This will allow the user to see that the data is being sent.
+                const loadingIcon = '<i slot="end" class="fa-solid fa-floppy-disk fa-fade" style="color: #bc204b;"></i>';
+                this.appendChild(document.createRange().createContextualFragment(loadingIcon));
+                // Create a promise to handle the asynchronous nature of the API call
+                // This will allow us to wait for the API response before proceeding
+                try {
+                    const result = await sendDataToAPI(name, value, dataTable, null);
+                    console.log(`Result from sendDataToAPI: ${result}`);
+                    // Check the result from the API call
+                    if (result === 'success') {
+                        this.removeAttribute('error'); // Remove the error state
+                        this.removeChild(this.querySelector('[slot="end"]'));
+                        this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                    } else {
+                        this.setAttribute('error', 'true'); // Set the error state
+                        this.removeChild(this.querySelector('[slot="end"]'));
+                        this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                    }
+                } catch (error) {
+                    console.error(`Error sending data to API: ${error}`);
+                    this.setAttribute('error', 'true'); // Set the error state
+                    this.removeChild(this.querySelector('[slot="end"]'));
+                    this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                }
+            } else {
+                // If the value has not changed, remove the loading icon if it exists
+                const loadingIcon = this.querySelector('[slot="end"]');
+                if (loadingIcon) {
+                    this.removeChild(loadingIcon);
+                }
+            }
+        });
+    });
+};
+
+const checkboxListeners = async () => {
+    const profileForm = document.getElementById('UserProfile');
+    const checkboxes = profileForm.querySelectorAll('input[type="checkbox"]');
+
+    checkboxes.forEach(checkbox => {
+        // Store the current checked state in a data attribute for later comparison
+        checkbox.setAttribute('data-original-checked', checkbox.checked);
+        // On change, if a change is detected, send the updated data to the server.
+        // This will allow the user to see that the data is being sent.
+        // Create a promise to handle the asynchronous nature of the API call
+        // This will allow us to wait for the API response before proceeding
+        checkbox.addEventListener('focus', function() {
+            // Store the current checked state in a data attribute for later comparison
+            this.setAttribute('data-original-checked', this.checked);
+        });
+
+        checkbox.addEventListener('change', async function() {
+            const originalValue = this.getAttribute('data-original-checked');
+            // Compare the current checked state with the original checked state
+            if (this.checked != originalValue) {
+                const name = this.name;
+                const value = this.checked ? '1' : '0'; // Convert checked state to string
+                const dataTable = this.getAttribute('data-table');
+                // Send the updated data to the server. Insert the loading icon into the element until the server responds.
+                // This will allow the user to see that the data is being sent.
+                const loadingIcon = '<i slot="end" class="fa-solid fa-floppy-disk fa-fade" style="color: #bc204b;"></i>';
+                this.parentElement.appendChild(document.createRange().createContextualFragment(loadingIcon));
+                try {
+                    const result = await sendDataToAPI(name, value, dataTable, null);
+                    console.log(`Result from sendDataToAPI: ${result}`);
+                    // Check the result from the API call
+                    if (result === 'success') {
+                        this.removeAttribute('error'); // Remove the error state
+                        this.parentElement.removeChild(this.querySelector('[slot="end"]'));
+                        this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                    } else {
+                        this.setAttribute('error', 'true'); // Set the error state
+                        this.parentElement.removeChild(this.querySelector('[slot="end"]'));
+                        this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                    }
+                } catch (error) {
+                    console.error(`Error sending data to API: ${error}`);
+                    this.setAttribute('error', 'true'); // Set the error state
+                    this.parentElement.removeChild(this.querySelector('[slot="end"]'));
+                    this.dispatchEvent(new Event('input')); // Trigger input event to update UI
+                }
+            } else {
+                // If the value has not changed, remove the loading icon if it exists
+                const loadingIcon = this.parentElement.querySelector('[slot="end"]');
+                if (loadingIcon) {
+                    this.parentElement.removeChild(loadingIcon);
+                }
+            }
+        });
+    });
 };
 
 inputListeners();
